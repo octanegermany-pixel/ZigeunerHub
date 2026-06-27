d669c641d14e01cdb67ee96392060a83cf8d4b8f
PashaChamp <octanegermany@gmail.com>
2026-06-27 22:57:48 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -26,7 +26,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "discord.gg/Zigeunerhub for the best Chea7s",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 5,
       },
     ],
   },
