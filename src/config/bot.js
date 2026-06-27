11300d7768798eba52249067a33256da88b04910
PashaChamp <octanegermany@gmail.com>
2026-06-27 22:59:32 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -91,8 +91,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#ffd700 ",
+      secondary: "#0000FF ",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
