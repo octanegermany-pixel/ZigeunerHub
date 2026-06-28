3aea9fe9ac2719c91453338f4e3aaa684a484586
PashaChamp <octanegermany@gmail.com>
2026-06-28 10:07:39 +0200

    Update vouch.js

--- src/commands/vouch.js
+++ src/commands/vouch.js
@@ -1,4 +1,10 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
+} = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,16 +15,19 @@ module.exports = {
 
         const embed = new EmbedBuilder()
             .setTitle("⭐ Vouch System")
-            .setDescription("Klicke auf den Button um ein Vouch zu schreiben!")
+            .setDescription("Klicke auf den Button und hinterlasse dein Feedback!")
             .setColor("Gold");
 
         const row = new ActionRowBuilder().addComponents(
             new ButtonBuilder()
                 .setCustomId("vouch_open")
-                .setLabel("Vouch schreiben")
+                .setLabel("⭐ Vouch schreiben")
                 .setStyle(ButtonStyle.Success)
         );
 
-        await interaction.reply({ embeds: [embed], components: [row] });
+        await interaction.reply({
+            embeds: [embed],
+            components: [row]
+        });
     }
 };
